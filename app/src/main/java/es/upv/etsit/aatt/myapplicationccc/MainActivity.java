520e2e2fc4832875ff76e74c44f45d42dc65b10d
Paco Martínez (Mac) <ppmm.mail@gmail.com>
2018-05-09 00:26:30 +0200

    lkjlkjhlkjasdfasdf

--- app/src/main/java/es/upv/etsit/aatt/myapplicationccc/MainActivity.java
+++ app/src/main/java/es/upv/etsit/aatt/myapplicationccc/MainActivity.java
@@ -3,7 +3,7 @@ package es.upv.etsit.aatt.myapplicationccc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
